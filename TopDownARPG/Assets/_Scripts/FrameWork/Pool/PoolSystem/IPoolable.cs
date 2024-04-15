146928dc7075295f899cbe8a8a25961dfb013477
sato <shatang821@gmail.com>
2024-04-15 14:11:48 +0900

    環境初期設定

---
+++ TopDownARPG/Assets/_Scripts/FrameWork/Pool/PoolSystem/IPoolable.cs
@@ -0,0 +1,14 @@
+﻿namespace FrameWork.Pool
+{
+    public interface IPoolable
+    {
+        /// <summary>
+        /// インスタンスが使用可能を示す
+        /// </summary>
+        bool IsActive { get; set; }
+        /// <summary>
+        /// リセット関数
+        /// </summary>
+        void Reset();
+    }
+}
---
+++ TopDownARPG/Assets/_Scripts/FrameWork/Utils/DebugLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace FrameWork.Utils
+{
+    public static class DebugLogger
+    {
+        public static void Log(object o)
+        {
+#if UNITY_EDITOR
+            UnityEngine.Debug.Log(o);
+#endif
+        }
+    } 
+}
