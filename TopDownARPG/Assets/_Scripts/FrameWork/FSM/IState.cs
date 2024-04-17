ac1a1e9908a71259362afc11254ed864cef64d31
sato <shatang821@gmail.com>
2024-04-17 14:05:28 +0900

    初期環境設定

--- TopDownARPG/Assets/_Scripts/FrameWork/FSM/IState.cs
+++ TopDownARPG/Assets/_Scripts/FrameWork/FSM/IState.cs
@@ -2,14 +2,14 @@ namespace FrameWork.FSM
 {
     public interface IState
     {
-        public void Enter();
+        public void Enter();            //初期処理
         
-        public void Exit();
+        public void Exit();             //終了処理
         
-        public void HandleInput();   
+        public void HandleInput();      //入力更新
         
-        public void LogicUpdate();
+        public void LogicUpdate();      //ロジック更新
         
-        public void PhysicsUpdate();
+        public void PhysicsUpdate();    //物理演算更新
     }
 }
