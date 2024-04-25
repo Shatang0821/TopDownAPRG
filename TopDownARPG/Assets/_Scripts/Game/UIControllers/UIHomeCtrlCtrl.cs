a2be268ce9bb260901a7ccfbfde2840a77bf1996
2067330293 <qq3266249918@gmail.com>
2024-04-25 12:55:46 +0900

    UICotrollerできた

---
+++ TopDownARPG/Assets/_Scripts/Game/UIControllers/UIHomeCtrlCtrl.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using FrameWork.UI;
+
+public class UIHomeCtrlCtrl : UICtrl
+{
+
+	public override void Awake() {
+
+		base.Awake();
+
+	}
+
+	void Start() {
+	}
+
+}
