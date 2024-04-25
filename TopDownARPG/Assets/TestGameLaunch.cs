f15cd85cf6bb08ade25ba6c8b60d0ea73d0dc0f5
2067330293 <qq3266249918@gmail.com>
2024-04-25 14:13:00 +0900

    panel出てくる

---
+++ TopDownARPG/Assets/TestGameLaunch.cs
@@ -0,0 +1,26 @@
+using FrameWork.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SK
+{
+    public class TestGameLaunch : MonoBehaviour
+    {
+        private void Awake()
+        {
+            UIManager.Instance.ShowUI("UIHomeCtrl");
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
--- TopDownARPG/Assets/_Scripts/Game/UIControllers/UIHomeCtrlCtrl.cs
+++ TopDownARPG/Assets/_Scripts/Game/UIControllers/UIHomeCtrlCtrl.cs
@@ -3,17 +3,47 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using FrameWork.UI;
+using Unity.VisualScripting;
 
 public class UIHomeCtrlCtrl : UICtrl
 {
+    public override void Awake()
+    {
+        base.Awake();
+        AddButtonListener("GameStart", GameStart);
+        AddButtonListener("Settings", Settings);
+        AddButtonListener("Operation", Operation);
+        AddButtonListener("Exit", Exit);
+        View["SettingsPanel"].SetActive(false);
+        View["OperationPanel"].SetActive(false);
+    }
 
-	public override void Awake() {
+    void Start()
+    {
+    }
 
-		base.Awake();
+    private void GameStart()
+    {
+        Debug.Log("GameStart");
+    }
 
-	}
+    private void Settings()
+    {
+        Debug.Log("Settings");
+        bool currentStatus = View["SettingsPanel"].activeSelf;
+        View["SettingsPanel"].SetActive(!currentStatus);
+    }
 
-	void Start() {
-	}
+    private void Operation()
+    {
+        Debug.Log("Operation");
+        bool currentStatus = View["OperationPanel"].activeSelf;
+        View["OperationPanel"].SetActive(!currentStatus);
+    }
 
+    private void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
 }
