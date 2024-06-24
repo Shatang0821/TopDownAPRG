063d7d73e1b59e39d4c814e9e81771b3f7c624e5
sck09040489018@icloud.com <sck09040489018@icloud.com>
2024-06-24 13:05:11 +0900

    Stage3 + Door

---
+++ TopDownARPG/Assets/DoorAnimationOnce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DoornimationOnce : MonoBehaviour
+{
+    private Animator anim;
+    private int animTriggerHash;
+    private bool hasPlayed;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        animTriggerHash = Animator.StringToHash("Door Animation");
+        anim.SetTrigger(animTriggerHash);
+
+        hasPlayed = false;
+    }
+
+    private void Update()
+    {
+        if (!hasPlayed && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            anim.enabled = false;
+            hasPlayed = true;
+        }
+    }
+}
