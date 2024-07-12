2886e26f7da18b16cf1bbd0edf77df9671714262
saitoshintaro <shintarosaito0617@gmail.com>
2024-07-12 12:40:29 +0900

    サーバーのURLの変更

--- TopDownARPG/Assets/_Scripts/Game/UIControllers/UILoginCtrl.cs
+++ TopDownARPG/Assets/_Scripts/Game/UIControllers/UILoginCtrl.cs
@@ -5,6 +5,7 @@ using FrameWork.UI;
 using FrameWork.Audio;
 using TMPro;
 using System.Collections;
+using System.IO;
 
 public class UILoginCtrl : UICtrl
 {
@@ -14,8 +15,20 @@ public class UILoginCtrl : UICtrl
         base.Awake();
 
         _api = FindObjectOfType<API>();
+        string filePath = Application.persistentDataPath + "/LoginAccount.json";
+        AccountManager accountmanager = new AccountManager(); // accountmanager を初期化
+        if (File.Exists(filePath))
+        {
+            // ファイルが存在する場合はロードして追加
+            string json = File.ReadAllText(filePath);
+            accountmanager = JsonUtility.FromJson<AccountManager>(json);
+            var accountname = View["Account"].GetComponent<TMP_InputField>();
+            var password = View["Password"].GetComponent<TMP_InputField>();
+            accountname.text = accountmanager.accountname;
+            password.text = accountmanager.password;
+        }
 
-        AddButtonListener("Register", Register);
+            AddButtonListener("Register", Register);
         AddButtonListener("SingIn", SingIn);
         AddButtonListener("RegistrationScreenPanel/Complete", Complete);
         AddButtonListener("RegistrationScreenPanel/Back", Back);
@@ -61,8 +74,11 @@ public class UILoginCtrl : UICtrl
         var password = View["Password"].GetComponent<TMP_InputField>();
         StartCoroutine(_api.Login(accountname, password));
 
-        StartCoroutine(SignIn());
-        
+        if (_api.isLogin)
+        {
+            StartCoroutine(SignIn());
+        }
+
 
     }
 
@@ -72,7 +88,11 @@ public class UILoginCtrl : UICtrl
         var password = View["RegistrationScreenPanel/Password (1)"].GetComponent<TMP_InputField>();
         StartCoroutine(_api.CreateAccount(accountname, password));
         Debug.Log("Complete");
-        StartCoroutine(SignIn());
+
+        if(!_api.isBadrequest)
+        {
+            StartCoroutine(SignIn());
+        }
 
     }
 
